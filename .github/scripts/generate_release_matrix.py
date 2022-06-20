9a154e96b3991ba7308c566f1f039cd80d5f05e7
Mathias Scherer <scherer.mat@gmail.com>
2022-06-20 17:17:39 +0200

    fix: adds back some label based work done by CI/CD

---
+++ .github/scripts/generate_release_matrix.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+try:
+    labels = json.loads(os.getenv('PULL_LABELS'))
+except:
+    labels = []
+
+matrix = []
+hasLabels = False
+for label in labels:
+    splitted = label['name'].split('-')
+    if len(splitted) == 2:
+        if splitted[1] == 'patch' or splitted[1] == 'minor' or splitted[1] == 'major':
+            matrix.append({
+                'package': splitted[0],
+            })
+            hasLabels = True
+
+print("::set-output name=hasLabels::" + str(hasLabels))
+print("::set-output name=matrix::" + json.dumps({'include': matrix}))
