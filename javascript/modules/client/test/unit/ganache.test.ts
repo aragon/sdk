33d01233496cd49ea86bcbe046f27cbe6ea8c0c4
josemarinas <josemarinasbascoy@gmail.com>
2022-08-19 17:22:31 +0200

    add client test

--- javascript/modules/client/test/unit/ganache.test.ts
+++ javascript/modules/client/test/unit/ganache.test.ts
@@ -1,7 +1,7 @@
 declare const describe, it, beforeAll, afterAll, expect;
 import * as ganacheSetup from "../../../../helpers/ganache-setup";
 import * as deployContracts from "../../../../helpers/deployContracts";
-import { ContextParams } from "../../src";
+import { Context, Client, ContextParams } from "../../src";
 import { Wallet } from "@ethersproject/wallet";
 import { JsonRpcProvider, Networkish } from "@ethersproject/providers";
 import { AddressZero } from "@ethersproject/constants";
@@ -11,6 +11,35 @@ import { Random } from "@aragon/sdk-common";
 const TEST_WALLET =
   "0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e";
 
+const IPFS_API_KEY = process.env.IPFS_API_KEY ||
+  Buffer.from(
+    "YjQ3N1JoRUNmOHM4c2RNN1hya0xCczJ3SGM0a0NNd3BiY0ZDNTVLdCAg==",
+    "base64",
+  ).toString().trim();
+
+const web3endpoints = {
+  working: [
+    "https://mainnet.infura.io/v3/94d2e8caf1bc4c4884af830d96f927ca",
+    "https://cloudflare-eth.com/",
+  ],
+  failing: ["https://bad-url-gateway.io/"],
+};
+
+const contextParams: ContextParams = {
+  network: "mainnet",
+  signer: new Wallet(TEST_WALLET),
+  daoFactoryAddress: "0x0123456789012345678901234567890123456789",
+  web3Providers: web3endpoints.working,
+  ipfsNodes: [
+    {
+      url: "https://testing-ipfs-0.aragon.network/api/v0",
+      headers: {
+        "X-API-KEY": IPFS_API_KEY || "",
+      },
+    },
+  ],
+  graphqlNodes: ["https://api.thegraph.com/subgraphs/name/aragon/aragon-zaragoza-rinkeby"]
+};
 const contextParamsLocalChain: ContextParams = {
   network: 31337,
   signer: new Wallet(TEST_WALLET),
@@ -44,9 +73,49 @@ describe("Client", () => {
   afterAll(async () => {
     await ganacheSetup.stop();
   });
-  it("Should Pass", async () => {
-    const a = 1
-    expect(a === 1).toBe(true)
+  describe("Client instances", () => {
+    it("Should create a working client", async () => {
+      const ctx = new Context(contextParams);
+      const client = new Client(ctx);
+
+      expect(client).toBeInstanceOf(Client);
+      expect(client.web3.getProvider()).toBeInstanceOf(JsonRpcProvider);
+      expect(client.web3.getConnectedSigner()).toBeInstanceOf(Wallet);
+
+      const web3Status = await client.web3.isUp();
+      expect(web3Status).toEqual(true);
+    });
+    it("Should create a failing client", async () => {
+      contextParams.web3Providers = web3endpoints.failing;
+      const context = new Context(contextParams);
+      const client = new Client(context);
+
+      expect(client).toBeInstanceOf(Client);
+      expect(client.web3.getProvider()).toBeInstanceOf(JsonRpcProvider);
+      expect(client.web3.getConnectedSigner()).toBeInstanceOf(Wallet);
+
+      const web3Status = await client.web3.isUp();
+      expect(web3Status).toEqual(false);
+    });
+    it("Should create a client, fail and shift to a working endpoint", async () => {
+      contextParams.web3Providers = web3endpoints.failing.concat(
+        web3endpoints.working,
+      );
+      const context = new Context(contextParams);
+      const client = new Client(context);
+
+      await client
+        .web3.isUp()
+        .then((isUp) => {
+          expect(isUp).toEqual(false);
+          client.web3.shiftProvider();
+
+          return client.web3.isUp();
+        })
+        .then((isUp) => {
+          expect(isUp).toEqual(true);
+        });
+    });
   });
 });
 
