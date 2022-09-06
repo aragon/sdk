0034c89793c46d55dc0382f1d17e4263cc40f386
josemarinas <josemarinasbascoy@gmail.com>
2022-09-06 12:12:18 +0200

    fix available functions constant

--- javascript/modules/client/src/internal/constants/encoding.ts
+++ javascript/modules/client/src/internal/constants/encoding.ts
@@ -1,5 +1,7 @@
+import { DAO__factory, MajorityVoting__factory } from "@aragon/core-contracts-ethers";
+
 export const AVAILABLE_FUNCTION_SIGNATURES: string[] = [
-  "function changeVoteConfig(uint64,uint64,uint64)",
-  "function withdraw(address,address,uint256,string)",
-  "function setMetadata(bytes)",
+  MajorityVoting__factory.createInterface().getFunction('changeVoteConfig').format('minimal'),
+  DAO__factory.createInterface().getFunction('withdraw').format('minimal'),
+  DAO__factory.createInterface().getFunction('setMetadata').format('minimal'),
 ];
