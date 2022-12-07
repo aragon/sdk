6572298e4a7cd1911fdbd3c3523aa50ca60b39a9
josemarinas <josemarinasbascoy@gmail.com>
2022-12-07 09:13:02 +0100

    add comment to isIpfsCid function

--- modules/client/src/client-common/utils.ts
+++ modules/client/src/client-common/utils.ts
@@ -76,6 +76,13 @@ export function isProposalId(propoosalId: string): boolean {
   return regex.test(propoosalId);
 }
 
+/**
+ * Checks is a string mathces an IPFS cid in both v0 and v1 versions
+ *
+ * @export
+ * @param {string} hash
+ * @return {*}  {boolean}
+ */
 export function isIpfsCid(hash: string): boolean {
   const regex = new RegExp(
     /^Qm([1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})$/,
