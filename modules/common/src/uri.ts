0c99065da32006d85450b7f111c622094aee473a
josemarinas <josemarinasbascoy@gmail.com>
2022-12-07 11:17:20 +0100

    add errors to the common package

--- modules/common/src/uri.ts
+++ modules/common/src/uri.ts
@@ -1,3 +1,5 @@
+import { InvalidCidError } from "./errors";
+
 const IPFS_CID_REGEX =
   /^Qm([1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})$/;
 
@@ -8,14 +10,14 @@ const PROTOCOL_REGEX = /^\w+:\/\//;
 export function resolveIpfsCid(data: string): string {
   if (!PROTOCOL_REGEX.test(data)) {
     if (!IPFS_CID_REGEX.test(data)) {
-      throw new Error("The value does not contain a valid CiD");
+      throw new InvalidCidError();
     }
     return data;
   }
 
   const metadataUri = new URL(data);
   if (!IPFS_CID_REGEX.test(metadataUri.host)) {
-    throw new Error("The value does not contain a valid CiD");
+    throw new InvalidCidError();
   }
   return metadataUri.host;
 }
