1f8bc8edb3036338ff8558ff3770de50290ae987
josemarinas <josemarinasbascoy@gmail.com>
2022-12-07 09:02:50 +0100

    fix metadata uri resolver

--- modules/common/src/uri.ts
+++ modules/common/src/uri.ts
@@ -1,11 +1,12 @@
 const IPFS_CID_REGEX =
   /^Qm([1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})$/;
 
+const PROTOCOL_REGEX = /^\w+:\/\//;
 /** Attempts to parse the given string as a URL and returns the IPFS CiD contained in it.
  * Alternatively it tries to use the raw value after validating it.
  */
 export function resolveIpfsCid(data: string): string {
-  if (!data.includes(":")) {
+  if (!PROTOCOL_REGEX.test(data)) {
     if (!IPFS_CID_REGEX.test(data)) {
       throw new Error("The value does not contain a valid CiD");
     }
