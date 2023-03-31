3883ea97ccf0526cf04492d87fe76d4af1d64df4
Mathias Scherer <mathias@aragon.org>
2023-03-31 17:02:49 +0200

    fix: ENS resolving error handling

--- modules/client/src/addresslistVoting/internal/client/methods.ts
+++ modules/client/src/addresslistVoting/internal/client/methods.ts
@@ -510,11 +510,15 @@ export class AddresslistVotingClientMethods extends ClientCore
         if (!provider) {
           throw new NoProviderError();
         }
-        const resolvedAddress = await provider.resolveName(address);
-        if (!resolvedAddress) {
+        try {
+          const resolvedAddress = await provider.resolveName(address);
+          if (!resolvedAddress) {
+            throw new InvalidAddressOrEnsError();
+          }
+          address = resolvedAddress;
+        } catch {
           throw new InvalidAddressOrEnsError();
         }
-        address = resolvedAddress;
       }
       where = { dao: address.toLowerCase() };
     }
