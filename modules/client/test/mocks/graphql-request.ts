6fb356ce1caaa19b8fda950190c53487caf9469c
Mathias Scherer <mathias@aragon.org>
2023-03-31 16:51:23 +0200

    fix: mocks gql properly in tests so we can check which queries are used

--- modules/client/test/mocks/graphql-request.ts
+++ modules/client/test/mocks/graphql-request.ts
@@ -1,4 +1,4 @@
-import { GraphQLClient } from "graphql-request";
+import { gql, GraphQLClient } from "graphql-request";
 
 jest.mock("graphql-request");
 
@@ -14,6 +14,14 @@ export function mockUpCheck(client: jest.Mocked<GraphQLClient>) {
 export function getMockedInstance(
   client: any, // needed to bypass wrong typecheck
 ): jest.Mocked<GraphQLClient> {
-  mockUpCheck(client);
   return client;
 }
+
+// Mocking gql template function
+const gqlMocked: jest.Mock = gql as jest.Mock; // typecasting here because we get already the mocked instance
+gqlMocked.mockImplementation(
+  // @ts-ignore Ignoring here because variables isn't in use
+  (chunks: TemplateStringsArray, ...variables: any[]) => {
+    return chunks[0]; // return the first entry. We always only have one chunk because we don't use advanced features of the gql template function
+  },
+);
