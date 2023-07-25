1e31ba96942007b0777befda3b69bf520cb3d94c
Michael Heuer <michael.heuer@rwth-aachen.de>
2023-07-25 14:08:22 +0200

    fix: wrong url

--- modules/client/examples/index.ts
+++ modules/client/examples/index.ts
@@ -77,7 +77,7 @@ export const contextParams: ContextParams = {
   // They will switch depending on the network (production, development)
   graphqlNodes: [
     {
-      url: "https://subgraph.satsuma-prod.com/aragon/core-goerli/api",
+      url: "https://subgraph.satsuma-prod.com/aragon/osx-goerli/api",
     },
   ],
 };
