c635f51c796dba806392bce35d66cca3dff11c5a
Jose Manuel Mariñas Bascoy <josemarinasbascoy@gmail.com>
2023-09-01 14:41:09 +0200

    fix regex recursivity

--- modules/common/src/utils.ts
+++ modules/common/src/utils.ts
@@ -13,7 +13,7 @@ export function isEnsName(name: string): boolean {
 
 export function isIpfsUri(cid: string): boolean {
   const regex = new RegExp(
-    /^ipfs:\/\/(Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})([/?#][-a-zA-Z0-9@:%_+.~#?&//=]*)*$/i,
+    /^ipfs:\/\/(Qm([1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,}))$/i,
   );
   return regex.test(cid);
 }
