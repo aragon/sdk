75f00e673778aae02323b179a6fbd4cafb2e2466
Jose Manuel Mariñas Bascoy <josemarinasbascoy@gmail.com>
2023-10-20 12:50:47 +0200

    update tests

--- modules/client-common/test/constants.ts
+++ modules/client-common/test/constants.ts
@@ -114,7 +114,7 @@ export const TEST_ADDRESS = "0x0000000000000000000000000000000000000001";
 export const TEST_INVALID_ADDRESS =
   "0x000000000000000000000000000000000000000P";
 
-export const TEST_ENS_NAME = "test.eth";
+export const TEST_ENS_NAME = "subdomain.test.eth";
 export const TEST_INVALID_ENS_NAME = "test.invalid";
 
 export const TEST_IPFS_CID_V0 =
