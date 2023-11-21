e7efab73c33b960ae9ec50a5b2b56fa15e1277d6
Mathias Scherer <mathias@aragon.org>
2023-11-21 12:55:15 +0100

    build(common): replace tsdx with eslint, jest and rollup build(common): updates typescript to v5

---
+++ modules/common/rollup.config.ts
@@ -0,0 +1,29 @@
+// rollup.config.js
+import typescript from "@rollup/plugin-typescript";
+import json from "@rollup/plugin-json";
+import terser from "@rollup/plugin-terser";
+import nodeResolve from "@rollup/plugin-node-resolve";
+
+export default {
+  input: "src/index.ts",
+  external: [/node_modules/],
+  output: [{
+    dir: "dist",
+    format: "cjs",
+    sourcemap: true,
+  }, {
+    file: "dist/sdk-common.esm.js",
+    format: "esm",
+    sourcemap: true,
+  }],
+  plugins: [
+    nodeResolve(),
+    typescript({
+      tsconfig: "./tsconfig.json",
+    }),
+    json(),
+    terser({
+      ecma: 2020,
+    }),
+  ],
+};
