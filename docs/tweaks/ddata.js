5ba93d39d50f284ea901a77470ba4311709d3679
banasa44 <cfbennassar@gmail.com>
2024-02-29 15:57:41 +0100

    ci: requested changes from code review

--- docs/tweaks/ddata.js
+++ docs/tweaks/ddata.js
@@ -1,34 +1,41 @@
-var util = require('util')
-var arrayify = require('array-back')
-exports.anchorName = anchorName
+const util = require("util");
+
+exports.anchorName = anchorName;
 
 function anchorName(options) {
-    var methodSign = options.data.methodSign ? options.data.methodSign.replace(/[^\w,]/g, '-').replace(/^-+|-+$/g, '').replace(/-+/g, '').replace(/,/g, '-').toLowerCase() : '';
+  const methodSign = options.data.methodSign
+    ? options.data.methodSign
+        .replace(/[^\w\s]/g, "")
+        .replace(/\s+/g, "-")
+        .toLowerCase()
+    : "";
 
-    if (!this.id) throw new Error('[anchorName helper] cannot create a link without a id: ' + JSON.stringify(this))
-    if (this.inherited) {
-      options.hash.id = this.inherits
-      var inherits = _identifier(options)
-      if (inherits) {
-        return anchorName.call(inherits, options)
-      } else {
-        return ''
-      }
+  if (!this.id)
+    throw new Error(
+      "[anchorName helper] cannot create a link without a id: " +
+        JSON.stringify(this)
+    );
+  if (this.inherited) {
+    options.hash.id = this.inherits;
+    const inherits = _identifier(options);
+    if (inherits) {
+      return anchorName.call(inherits, options);
+    } else {
+      return "";
     }
-
-    return util.format(
-        '%s%s%s%s',
-        this.isExported ? 'exp_' : '',
-        this.kind === 'constructor' ? 'new_' : '',
-        this.id
-          .replace(/:/g, '_')
-          .replace(/~/g, '..')
-          .replace(/\(\)/g, '_new')
-          .replace(/#/g, '')
-          .replace(/\./g, '')
-          .toLowerCase(), 
-          methodSign
-      )
-      
   }
-  
+
+  return util.format(
+    "%s%s%s%s",
+    this.isExported ? "exp_" : "",
+    this.kind === "constructor" ? "new_" : "",
+    this.id
+      .replace(/:/g, "_")
+      .replace(/~/g, "..")
+      .replace(/\(\)/g, "_new")
+      .replace(/#/g, "")
+      .replace(/\./g, "")
+      .toLowerCase(),
+    methodSign
+  );
+}
--- docs/tweaks/helpers.js
+++ docs/tweaks/helpers.js
@@ -1,19 +1,19 @@
-exports.toLowerCase = toLowerCase
-exports.stripTags = stripTags
-exports.extractText = extractText
+exports.toLowerCase = toLowerCase;
+exports.stripTags = stripTags;
+exports.extractText = extractText;
 
 function stripTags(input) {
-    // Replace HTML entities with their corresponding characters
-    var decodedInput = input.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
-    // Remove <p> tags
-    return decodedInput.replace(/<\/?p>/ig, "");
+  // Replace HTML entities with their corresponding characters
+  const decodedInput = input.replace(/&lt;/g, "<").replace(/&gt;/g, ">");
+  // Remove <p> tags
+  return decodedInput.replace(/<\/?p>/gi, "");
 }
 
-function toLowerCase(input){
-    return input.toLowerCase();
+function toLowerCase(input) {
+  return input.toLowerCase();
 }
 
 function extractText(input) {
-    // Keep only alphabetic characters (a-z, A-Z)
-    return input.match(/[a-zA-Z0-9]+/g).join("");
+  // Keep only alphabetic characters (a-z, A-Z)
+  return input.match(/[a-zA-Z0-9]+/g).join("");
 }
